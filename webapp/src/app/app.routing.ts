bbd440114e9196d023d8ce110ca38fd0f4b952c6
Nirali Tank <nirali.tank@INFXINC.NET>
2017-02-10 14:48:25 -0800

    Adding Footer files

--- webapp/src/app/app.routing.ts
+++ webapp/src/app/app.routing.ts
@@ -2,6 +2,7 @@ import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import {Home} from './components/home/home';
+import {Signup} from './components/signup/signup';
 
 const appRoutes: Routes = [
   { path: '**', component: Home },
---
+++ webapp/src/app/components/register/signup.ts
@@ -0,0 +1,17 @@
+import {Component} from '@angular/core';
+import {Router} from '@angular/router';
+
+
+@Component({
+  selector: 'signup',
+  templateUrl: './signup.html',
+  styleUrls: ['./signup.css'],
+  providers: [],
+})
+
+export class Signup {
+
+	constructor( private router: Router) {}
+
+ 
+}
