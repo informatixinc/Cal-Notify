4a549d8978c674371533361376a2f8be7b878e51
Nirali Tank <nirali.tank@INFXINC.NET>
2017-02-15 14:48:32 -0800

    adding dropdown menu for state in signup page

--- webapp/src/app/components/signup/signup.ts
+++ webapp/src/app/components/signup/signup.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import {Router} from '@angular/router';
 import {LanguageService} from '../../services/language/language_service';
+import {SignupObject} from './signup_object';
 
 @Component({
   selector: 'signup',
@@ -11,7 +12,12 @@ import {LanguageService} from '../../services/language/language_service';
 
 export class Signup {
 
+	states = ["CA", "AL", "AK", "AZ", "AR", "CO", "CT", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"];
+	sign_up: SignupObject = new SignupObject();
 	constructor(private router: Router, private _languageService: LanguageService) {}
+	signup(){
 
- 
+		
+	}
+ 	
 }
