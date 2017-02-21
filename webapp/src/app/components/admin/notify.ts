2151f4cbe482ce6d6f5daa1f4729266e9e7d52ec
Nirali Tank <nirali.tank@INFXINC.NET>
2017-02-21 14:21:02 -0800

    Updating Notification Management layout for Admin

--- webapp/src/app/components/admin/notify.ts
+++ webapp/src/app/components/admin/notify.ts
@@ -1,6 +1,8 @@
 import {Component} from '@angular/core';
 import {Router} from '@angular/router';
 import {LanguageService} from '../../services/language/language_service';
+import {NotifyObject} from './notify_object';
+import {NotifyObjectError} from './notify_error';
 
 @Component({
   selector: 'notify',
@@ -11,7 +13,30 @@ import {LanguageService} from '../../services/language/language_service';
 
 export class Notify {
 
+	notify: NotifyObject = new NotifyObject();
+	error: NotifyObjectError = new NotifyObjectError();
 	constructor(private router: Router, private _languageService: LanguageService ) {}
 
+	send(){
+
+		this.error.notifyTitle = this.error.expDate = this.error.notifyDetails = "";
+
+		document.getElementById("title").style["borderColor"] = "black";
+		document.getElementById("expdate").style["borderColor"] = "black";
+		document.getElementById("message").style["borderColor"] = "black";
+		
+		if(this.notify.notifyTitle.length == 0){
+			this.error.notifyTitle = this._languageService.getTranslation("title_required");
+			document.getElementById("title").style["borderColor"] = "#CD2026";
+		}
+		if(this.notify.expDate.length == 0){
+			this.error.expDate = this._languageService.getTranslation("date_required");
+			document.getElementById("expdate").style["borderColor"] = "#CD2026";
+		}
+		if(this.notify.notifyDetails.length == 0){
+			this.error.notifyDetails = this._languageService.getTranslation("message_required");
+			document.getElementById("message").style["borderColor"] = "#CD2026";
+		}
+	}
  
 }
---
+++ webapp/src/app/components/admin/notify_object.ts
@@ -0,0 +1,5 @@
+export class NotifyObject {
+	notifyTitle: string = "";
+	expDate: string = "";
+	notifyDetails: string = "";
+}
