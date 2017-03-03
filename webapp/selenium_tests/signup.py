ced8a5a94d95f7c9667df6e62c821f7538ec3c47
Sean <skammerich@gmail.com>
2017-03-02 18:06:21 -0800

    Updating selenium tests

--- webapp/selenium_tests/signup.py
+++ webapp/selenium_tests/signup.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 import common, time
 
-def signup(fname, lname, address1, address2, city, state, zipcode, phone, email, password, confPassword, driver):
+def signup(fname, lname, addressOne, addressTwo, city, state, zipcode, phone, email, password, driver):
 
 	inputs = []
 
@@ -11,15 +11,15 @@ def signup(fname, lname, address1, address2, city, state, zipcode, phone, email,
 
 	inputs[0].send_keys(fname)
 	inputs[1].send_keys(lname)
-	inputs[2].send_keys(address1)
-	inputs[3].send_keys(address2)
+	inputs[2].send_keys(addressOne)
+	inputs[3].send_keys(addressTwo)
 	inputs[4].send_keys(city)
 	driver.find_element_by_xpath("//select[@id='state']/option[text()='CA']").click()
 	inputs[5].send_keys(zipcode)
 	inputs[6].send_keys(phone)
 	inputs[7].send_keys(email)
 	inputs[8].send_keys(password)
-	inputs[9].send_keys(confPassword)
+	inputs[9].send_keys(password)
 
 	button = driver.find_element_by_id('submit')
 	button.click()
--- webapp/selenium_tests/test_suite_runner.py
+++ webapp/selenium_tests/test_suite_runner.py
@@ -6,31 +6,46 @@ import login, dashboard, signup, addlocations, updateaccount
 
 baseUrl = "http://localhost:3000/"
 
-def doRun(email, password, updateFname, updateLname, updateAddress1, updateAddress2, updateCity, updateState, updateZipcode, updatePhone, updateEmail, updateOldPassword, updatePassword, updateConfPassword):
+email = "test@gmail.com"
+password = "Password1"
+fname = "first"
+updateFname = "first updated"
+lname = "last"
+updateLname = "last updated"
+addressOne = "2485 Natomas Park Dr"
+addressTwo = "suite #430"
+city = "Sacramento"
+state = "CA"
+zipCode = "95833"
+phoneNumber = "4086948765"
+nickname = "test location"
+
+def runTests():
 	binary = FirefoxBinary()
 	driver = webdriver.Firefox(firefox_binary=binary)
 
 	#Signup
-	driver.get(baseUrl+"signup")
-	signup.signup(fname, lname, address1, address2, city, state, zipcode, phone, email, password, confPassword, driver)
+	# driver.get(baseUrl+"signup")
+	# signup.signup(fname, lname, addressOne, addressTwo, city, state, zipCode, phoneNumber, email, password, driver)
 
 	#Login
 	driver.get(baseUrl+"home")
 	login.doLogin(email,password,driver)
+	
 	# dashboard.doDashboard(driver)
 
-	#Add new address location
+	# #Add new address location
 	driver.get(baseUrl+"notification")
 	addlocations.addLoc(nickname, address, city, state, zipcode, driver)
 	addlocations.addLoc(driver)
 
-	#Update account information
-	updateaccount.account(updateFname, updateLname, updateAddress1, updateAddress2, updateCity, updateState, updateZipcode, updatePhone, updateEmail, updateOldPassword, updatePassword, updateConfPassword, driver)
-	dashboard.doDashboard(driver)
-	driver.close()
+	# #Update account information
+	# updateaccount.account(updateFname, updateLname, updateAddress1, updateAddress2, updateCity, updateState, updateZipcode, updatePhone, updateEmail, updateOldPassword, updatePassword, updateConfPassword, driver)
+	# dashboard.doDashboard(driver)
+	# driver.close()
 
 def main():
-	doRun("test@gmail.com", "Password1", "first", "last", "2485 Natomas Park Dr", "suite #430", "Sacramento", "CA", "95833", "4086948765", "test@gmail.com", "Password1", "Password12", "Password12")
+	runTests()
 	print ("Test suite has executed successfully!")
 
 if __name__ == "__main__":
